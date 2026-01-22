b9c48a096abe02bfb11d80626f6f06a0287e5fe4
Vitaly K. <kaid.vit@gmail.com>
2026-01-22 22:58:22 +0300

    npm: @wxt-dev/auto-icons; Update icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/auto-icons"],
   manifest: {
+    name: "CrowProx",
     permissions: ["contextMenus", "proxy", "storage", "activeTab", "tabs"],
     action: {},
   },
